a58178075155a843b6e606f1a4dc3d46fac8de6b
karobia001 <karobiamaina81@gmail.com>
2019-10-21 12:53:00 +0300

    we create a user class that will help us create new user and pass in db model as an argument

---
+++ app/models.py
@@ -0,0 +1,22 @@
+from . import db
+
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+
+class User(UserMixin,db.Model):
+    
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255))
+   
+
+
+   
+
+    def __repr__(self):
+        return f'{self.user_id}:{self.pitch_id}'

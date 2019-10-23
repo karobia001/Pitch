a0ef03ed5d31e4d63884d1e4d2982fec580ceade
karobia001 <karobiamaina81@gmail.com>
2019-10-23 11:53:55 +0300

    we create a user class that will help us create new users this will connect our class to our database

--- app/models.py
+++ app/models.py
@@ -1,69 +1,22 @@
 from . import db,login_manager
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(,db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index=True)
+    email = db.Column(db.String(255),unique=True,index=True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())    
+    password_hash = db.Column(db.String(255))
+    pitch_id = db.relationship('Pitch',backref='user',lazy="dynamic")
+
+   
+    def __repr__(self):
+        return f'User {self.username}'
 
 
 
-class Pitch(db.Model):
-    __tablename__ = 'pitch'
-    id = db.Column(db.Integer,primary_key=True)
-    title = db.Column(db.String)
-    name = db.Column(db.String)
-    category = db.Column(db.String)
-    pitch = db.Column(db.String)
-    posted = db.Column(db.DateTime,default=datetime.utcnow)
-    user_id=db.Column(db.Integer,db.ForeignKey("users.id"))
-    comments = db.relationship('Comment',backref='pitchez',lazy="dynamic")
-
-    def save_pitch(self):
-        db.session.add(self)
-        db.session.commit()
-
-    @classmethod
-    def clear_pitch(cls):
-        Pitch.all_pitch.clear()
-
-    @classmethod
-    def get_category(cls,category):
-        pitch_cat = Pitch.query.filter_by(category=category).all()
-        return pitch_cat
-
-    all_pitch=[]
-    def __init__(self,title,name,category,pitch,user):
-        self.title=title
-        self.name = name
-        self.category = category
-        self.pitch = pitch
-        self.user =user
-
-class Comment(db.Model):
-
-    __tablename__ = 'comments'
-    id =db.Column(db.Integer, primary_key=True)
-    author = db.Column(db.String)
-    body = db.Column(db.String)
-    vote = db.Column(db.String)
-    pitch_id = db.Column(db.Integer,db.ForeignKey("pitch.id"))
-
-    all_comments = []
-    def __init__(self,author,body,vote):
-        self.body = body
-        self.vote = vote
-        self.author = author
-
-    def save_comment(self):
-        db.session.add(self)
-        db.session.commit()
-
-    @classmethod
-    def clear_comment(cls):
-        Comment.all_comments.clear()
-
-    @classmethod
-    def get_comments(cls):
-        comments = Comment.query.all()
-        return comments
-        # response = []
-        # for comment in cls.all_comments:
-        #         response.append(comment)
-
-         return response

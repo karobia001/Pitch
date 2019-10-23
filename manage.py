3b4e8d0b25ded233b16aa6ca361ef165f2bcaba1
karobia001 <karobiamaina81@gmail.com>
2019-10-23 11:59:28 +0300

    we use manager decorator to create a shell context

--- manage.py
+++ manage.py
@@ -1,7 +1,11 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager,Server
 from app.models import User,Pitch
 
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app,db=db,User=User,Pitch=Pitch)
 
 if __name__ == '__main__':
     manager.run()
